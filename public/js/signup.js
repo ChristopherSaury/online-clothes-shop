17083c2e576a65323b06958b48c4b9edcb2fb1ad
ChristopherSaury <saurychristopher@gmail.com>
2023-01-05 10:19:47 +0100

    sign up form validation

---
+++ public/js/signup.js
@@ -0,0 +1,53 @@
+let signupForm = document.querySelector('#signup #signupForm');
+let errMsg = document.querySelector('#signup #signupForm .errorSignUp .errSignUpMsg');
+let errMsgContainer = document.querySelector('#signup #signupForm .errorSignUp');
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+function signup(){
+   if(signupForm['email-address'].value === '' || signupForm['password'].value === ''
+    || signupForm['password2'].value === '' || signupForm['lastname'].value === ''
+    || signupForm['firstname'].value === '' || signupForm['country'].value === ''
+    || signupForm['address'].value === '' || signupForm['city'].value === ''
+    || signupForm['postcode'].value === '' || signupForm['phone'].value === ''
+    || signupForm['birth-date'].value === ''){
+    
+        return errorSignUpRedirect('You must fill in all the required fields*');
+
+   }else if(signupForm['password'].value != signupForm['password2'].value){
+        return errorSignUpRedirect('The field Password and the field Confirm Password must be identical')
+   }else if(signupForm['password'].value.length < 8 || signupForm['password'].value.length > 30){
+    return errorSignUpRedirect('The password must be between 8 and 30 characters');
+   }else if (!signupForm['email-address'].value.match(pattern)){
+        return errorSignUpRedirect('Please Enter Valid Email Address');
+   }else if (signupForm['lastname'].value.length < 2 || signupForm['lastname'].value.length > 30
+    || signupForm['firstname'].value.length < 2 || signupForm['firstname'].value.length > 30){
+    return errorSignUpRedirect('Lastname and Firstname must be between 2 and 30 characters');
+   }else if (!signupForm['termsOfUse'].checked){
+    return errorSignUpRedirect('You must accept terms of use');
+}
+
+    let signupData = document.querySelectorAll('#signup #signupForm .signupData');
+    let form_data = new FormData();
+
+    for(let count = 0; count < signupData.length; count++){
+        form_data.append(signupData[count].name , signupData[count].value);
+    }
+    document.querySelector('#signup #signupForm #signupSubmit').disabled = true;
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('POST', '/signup/handler');
+    xhttp.send(form_data);
+
+    
+}
+
+function errorSignUpRedirect(message){
+    window.location.href = '#signupForm';
+    errMsg.innerHTML = message;
+    document.documentElement.scrollTop = 0 ;
+    return errMsgContainer.style.display = 'flex';
+}
+
+function closeErrMsgSignUp(){
+    return errMsgContainer.style.display = 'none';
+}
