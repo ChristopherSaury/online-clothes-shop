fef1cb185d47613e984d50f29ec3851890f7436f
ChristopherSaury <saurychristopher@gmail.com>
2023-01-05 10:18:30 +0100

    country new properties

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CountryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CountryRepository::class)]
@@ -19,6 +21,14 @@ class Country
     #[ORM\ManyToOne(inversedBy: 'countries')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Continent $continent = null;
+
+    #[ORM\OneToMany(mappedBy: 'country', targetEntity: User::class)]
+    private Collection $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +55,36 @@ class Country
     public function setContinent(?Continent $continent): self
     {
         $this->continent = $continent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users->add($user);
+            $user->setCountry($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getCountry() === $this) {
+                $user->setCountry(null);
+            }
+        }
 
         return $this;
     }
