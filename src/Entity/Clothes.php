c3df7ac91015a9a9bd022fe51e53a4cf03309efd
ChristopherSaury <saurychristopher@gmail.com>
2023-01-07 10:17:25 +0100

    Clothes entity

---
+++ src/Entity/Clothes.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClothesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClothesRepository::class)]
+class Clothes
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'clothes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Brand $brand = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $model = null;
+
+    #[ORM\ManyToOne(inversedBy: 'clothes')]
+    private ?ItemCollection $clothe_collection = null;
+
+    #[ORM\ManyToOne(inversedBy: 'clothes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ClothesCategory $category = null;
+
+    #[ORM\ManyToOne(inversedBy: 'clothes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Gender $gender = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: '0')]
+    private ?string $price = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?Brand
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(?Brand $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(?string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getClotheCollection(): ?ItemCollection
+    {
+        return $this->clothe_collection;
+    }
+
+    public function setClotheCollection(?ItemCollection $clothe_collection): self
+    {
+        $this->clothe_collection = $clothe_collection;
+
+        return $this;
+    }
+
+    public function getCategory(): ?ClothesCategory
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?ClothesCategory $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getGender(): ?Gender
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?Gender $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
