f1ec9d8b49f866aafd60e5c13afe0705492c5fcb
ChristopherSaury <saurychristopher@gmail.com>
2023-02-02 15:05:24 +0100

    homepage product

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\ClothesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StaticController extends AbstractController{
     #[Route('/', name:'home')]
-    public function homePage(){
-        return $this->render('static/home.html.twig');
+    public function homePage(ClothesRepository $clothe){
+        $home_product = [$clothe->find(26), $clothe->find(31), $clothe->find(19), $clothe->find(22)];
+        return $this->render('static/home.html.twig', compact('home_product'));
     }
 
     #[Route('/about', name:'about')]
